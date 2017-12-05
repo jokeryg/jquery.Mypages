b1b6f9c13bdac63508fdabad61fd80228c48afca
jokeryg <smallblood@gmail.com>
2017-12-05 15:55:29 +0800

    1.0

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
 # jquery.Mypages
 基于Jquery的分页插件
+
+###使用方法
+```html
+<html>
+    <link href="myPages.css" rel="stylesheet" type="text/css"/>
+    <script src="myPages.js" type="text/javascript"></script>
+    
+    <script type="text/javascript">
+        $(document).ready(function(){
+            $("#myPages").myPages({
+                pageNo:3,
+                pageSize:15,
+                firstPage:1,
+                lastPage:10,
+                url: "/list/{pageNo}-{pageSize}",
+            })
+        })
+    </script>
+    
+<body>
+    <div id="myPages">
+    </div>
+</body>
+</html>
+```
+
+
+###参数说明
+```javascript
+    var defaluts = {
+        url: "/{pageNo}-{pageSize}",   //页码跳转链接 {pageNo}与{pageSize}为固定占位符
+        pageNo: 1,      //当前页码,同时对应链接内 {pageNo}
+        pageSize: 10,   //每页显示数量,对应链接内{pageSize}
+        firstPage: 1,   //首页页码
+        lastPage: 1,   //最后一页页码
+        preText: "<",   //上一页显示的文字，默认为 <
+        nextText: ">",  //下一页显示的文字，默认为 >
+        firstText: "<<",//首页显示的文字,默认为 <<
+        lastText: ">>", //最后一页显示的文字,默认为 >>
+        showPageButton: 6, //显示的页码数量，默认为6
+        isMobile:false   //此值为true时只显示上一页与下一页,默认为false
+    };
+```
---
+++ myPages.js
@@ -0,0 +1,94 @@
+//闭包限定命名空间
+(function ($) {
+    $.fn.extend({
+        "myPages": function (options) {
+            var opts = $.extend({}, defaluts, options); 
+            this.each(function () {  
+                var url = opts.url;
+                var pageNo = opts.pageNo
+                var pageSize = opts.pageSize
+                var firstPage = opts.firstPage
+                var lastPage = opts.lastPage
+                var preText = opts.preText
+                var nextText = opts.nextText
+                var firstText = opts.firstText
+                var lastText = opts.lastText
+                var showPageButton = opts.showPageButton
+                var isMobile = opts.isMobile
+                url = url.replaceAll("{pageSize}", pageSize);
+                var prePage = pageNo == firstPage?pageNo:pageNo-1;
+                var nextPage = pageNo == lastPage?pageNo:pageNo+1;
+                //生成HTML
+                var html = "<nav><ul class='myPages'>";
+                if(!isMobile){
+                    html+= "<li><a href='" + url.replaceAll("{pageNo}", firstPage) + "'> "+firstText+" </a></li>";
+                }
+                if (pageNo == firstPage) {
+                    //html += "<li  class='disabled'><a class='noClickPage' href='javascript:void(0)'>上一页</a></li>";
+                } else {
+                    html += "<li><a href='" + url.replaceAll("{pageNo}", prePage) + "'>"+preText+"</a></li>";
+                }
+                if(!isMobile) {
+                    //计算循环
+                    var temp = showPageButton / 2;
+                    var begin = firstPage;
+                    var end = pageNo + temp;
+                    if (end > lastPage) {
+                        end = lastPage;
+                    }
+                    if (pageNo - temp > firstPage) {
+                        begin = pageNo - temp;
+                    }
+                    if (end - showPageButton > firstPage && end - begin < showPageButton) {
+                        begin = end - showPageButton;
+                    }
+
+                    if (end - begin < showPageButton && begin + showPageButton < lastPage) {
+                        end = begin + showPageButton;
+                    }
+
+                    var counts = 0;
+                    for (var i = begin; i <= end; i++) {
+                        counts++;
+                        if (i == pageNo) {
+                            html += "<li  class='active'><a class='noClickPage' href='javascript:void(0)'>" + pageNo + "</a></li>";
+                        } else {
+
+                            html += "<li><a href='" + url.replaceAll("{pageNo}", i) + "'>" + i + "</a></li>";
+                        }
+                    }
+                }
+                if (pageNo == lastPage) {
+                    //html += "<li class='disabled'><a class='noClickPage' href='javascript:void(0)'>下一页</a></li>";
+                } else {
+                    html += "<li><a href='" + url.replaceAll("{pageNo}", nextPage) + "'>"+nextText+"</a></li>";
+                }
+                if(!isMobile) {
+                    html += "<li><a href='" + url.replaceAll("{pageNo}", lastPage) + "'> "+lastText+" </a></li>";
+                }
+                html+="</ul></nav>";
+
+                $(this).append(html);
+                $(this).addClass("myPages");
+            });
+
+        }
+    });
+    //默认参数
+    var defaluts = {
+        pageNo: 1,
+        pageSize: 10,
+        firstPage: 1,
+        lastPage: 1,
+        preText: "<",
+        nextText: ">",
+        firstText: "<<",
+        lastText: ">>",
+        url: "/{pageNo}-{pageSize}",
+        showPageButton: 6,
+        isMobile:false
+    };
+})(window.jQuery);
+String.prototype.replaceAll = function (s1, s2) {
+    return this.replace(new RegExp(s1, "gm"), s2);
+}
